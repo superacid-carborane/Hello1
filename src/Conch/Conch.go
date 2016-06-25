cebe1828f69675cea4158d0096be873efead7bf7
Carborane <anastasia@belenky.net>
2016-06-25 15:43:12 -0700

    Added conch

---
+++ src/Conch/Conch.go
@@ -0,0 +1,32 @@
+//stole this from https://www.golang-book.com/books/intro/10 . Going to heavily modify it. Teaching myself to use channels and goroutines
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func pinger(c chan string) {
+  for i := 0; ; i++ {
+    c <- "ping"
+  }
+}
+
+func printer(c chan string) {
+  for {
+    msg := <- c
+    fmt.Println(msg)
+    time.Sleep(time.Second * 1)
+  }
+}
+
+func main() {
+  var c chan string = make(chan string)
+
+  go pinger(c)
+  go printer(c)
+
+  var input string
+  fmt.Scanln(&input)
+}
